4eb38646baad2c677d4733b2f9f986809bdfcd5b
EPCVivares <94907335+EPCVivares@users.noreply.github.com>
2022-10-05 23:48:31 +0200

    He cambiado la división por cero y he creado el método de introducir los números para no repetir tanto código. Aún así, lo he comentado

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -17,54 +17,74 @@ public class MainActivity extends AppCompatActivity {
     private EditText numeroUno;
     private EditText numeroDos;
     private int resultado;
+    private int num1;
+    private int num2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         numeroUno = findViewById(R.id.operando1);//Variable que guarda el numero 1
         numeroDos = findViewById(R.id.operando2);//Variable que guarda el numero 2
 
-    }
+    } // Fin onCreate
 
 
     public void suma (View v){
-        int num1 = Integer.parseInt(numeroUno.getText().toString());
-        int num2 = Integer.parseInt(numeroDos.getText().toString());
+     //   int num1 = Integer.parseInt(numeroUno.getText().toString());
+     //   int num2 = Integer.parseInt(numeroDos.getText().toString());
+        cargarNumeros();
         resultado = num1+num2;
         mostrar();
-    }
+    } //Fin suma
+
 
     private void mostrar() {
 
         textoResultado = findViewById(R.id.resultado);
         textoResultado.setText(""+resultado);
 
-    }
+    } //Fin mostrar
+
 
     public void resta (View v){
-        int num1 = Integer.parseInt(numeroUno.getText().toString());
-        int num2 = Integer.parseInt(numeroDos.getText().toString());
+      //  int num1 = Integer.parseInt(numeroUno.getText().toString());
+     //   int num2 = Integer.parseInt(numeroDos.getText().toString());
+        cargarNumeros();
         resultado = num1-num2;
         mostrar();
-    }
+    } //Fin restar
+
 
     public void multiplicacion (View v){
-        int num1 = Integer.parseInt(numeroUno.getText().toString());
-        int num2 = Integer.parseInt(numeroDos.getText().toString());
+      //  int num1 = Integer.parseInt(numeroUno.getText().toString());
+      //  int num2 = Integer.parseInt(numeroDos.getText().toString());
+        cargarNumeros();
         resultado = num1*num2;
         mostrar();
-    }
+    }//Fin multiplicación
+
 
     public void division (View v){
-        int num1 = Integer.parseInt(numeroUno.getText().toString());
-        int num2 = Integer.parseInt(numeroDos.getText().toString());
+       // int num1 = Integer.parseInt(numeroUno.getText().toString());
+      //  int num2 = Integer.parseInt(numeroDos.getText().toString());
+        cargarNumeros();
         if (num2!=0){
             resultado = num1/num2;
+            mostrar();
         }
-        resultado = num1/num2;
-        mostrar();
-    }
-}
+        else { //Si el segundo número es 0 da un error, porque no se puede dividir entre cero
+            textoResultado.setText("ERROR");
+        }
+       // resultado = num1/num2;
+    }// Fin división
+
+
+    public void cargarNumeros(){
+        num1 = Integer.parseInt(numeroUno.getText().toString());
+        num2 = Integer.parseInt(numeroDos.getText().toString());
+    }//Fin cargarNumeros
+
+}//Fin MainActivity
