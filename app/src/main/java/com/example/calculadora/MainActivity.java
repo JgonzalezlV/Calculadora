01bb222e6b82e8c6e9c4fd579e177ac29010fd76
Josu González <gonzalezlopezjosu@gmail.com>
2022-10-06 11:37:11 +0200

    Commit Spinner

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,21 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    /*private Button suma;
-    private Button division;
-    private Button resta;
-    private Button multiplicacion;*/
+public class MainActivity<Rounded> extends AppCompatActivity {
+
     private TextView textoResultado;
     private EditText numeroUno;
     private EditText numeroDos;
-    private int resultado;
-    private int num1;
-    private int num2;
+    private float resultado;
+    private double num1;
+    private double num2;
+    private Spinner spinner;
+    private String[] operaciones = {"","Suma","Resta","Multiplicacion","Division"};
 
 
     @Override
@@ -28,17 +29,55 @@ public class MainActivity extends AppCompatActivity {
 
         numeroUno = findViewById(R.id.operando1);//Variable que guarda el numero 1
         numeroDos = findViewById(R.id.operando2);//Variable que guarda el numero 2
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,operaciones);
+        spinner.setAdapter(adapter);
 
     } // Fin onCreate
 
 
-    public void suma (View v){
-     //   int num1 = Integer.parseInt(numeroUno.getText().toString());
-     //   int num2 = Integer.parseInt(numeroDos.getText().toString());
+    public void operaciones(View v){
+
         cargarNumeros();
-        resultado = num1+num2;
-        mostrar();
-    } //Fin suma
+        comprobarOperaciones();
+
+
+    }
+
+    private void comprobarOperaciones() {
+
+        String operacion = spinner.getSelectedItem().toString();
+        if (operacion.equals("Suma")){
+            resultado = (float) (num1+num2);
+            mostrar();
+        }else if (operacion.equals("Resta")){
+            resultado = (float) (num1-num2);
+            mostrar();
+        }else if (operacion.equals("Multiplicacion")){
+            resultado = (float) (num1*num2);
+            mostrar();
+        }else if (operacion.equals("Division")) {
+            if (num2 != 0) {
+                resultado = (float) (num1 / num2);
+                mostrar();
+            } else { //Si el segundo número es 0 da un error, porque no se puede dividir entre cero
+                textoResultado.setText("ERROR");
+            }
+        }
+    }
+
+    /*public void suma (View v){
+
+        cargarNumeros();
+        String operacion = spinner.getSelectedItem().toString();
+        if (!operacion.equals("") && !operacion.equals("Resta") && !operacion.equals("Multiplicacion") && !operacion.equals("Division")){
+            resultado = num1+num2;
+            mostrar();
+        }else {
+            resultado = 0;
+        }
+
+    } //Fin suma*/
 
 
     private void mostrar() {
@@ -49,9 +88,8 @@ public class MainActivity extends AppCompatActivity {
     } //Fin mostrar
 
 
-    public void resta (View v){
-      //  int num1 = Integer.parseInt(numeroUno.getText().toString());
-     //   int num2 = Integer.parseInt(numeroDos.getText().toString());
+    /*public void resta (View v){
+
         cargarNumeros();
         resultado = num1-num2;
         mostrar();
@@ -59,8 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void multiplicacion (View v){
-      //  int num1 = Integer.parseInt(numeroUno.getText().toString());
-      //  int num2 = Integer.parseInt(numeroDos.getText().toString());
+
         cargarNumeros();
         resultado = num1*num2;
         mostrar();
@@ -68,18 +105,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void division (View v){
-       // int num1 = Integer.parseInt(numeroUno.getText().toString());
-      //  int num2 = Integer.parseInt(numeroDos.getText().toString());
+
         cargarNumeros();
         if (num2!=0){
-            resultado = num1/num2;
+            resultado = Double.valueOf(num1/num2);
             mostrar();
         }
         else { //Si el segundo número es 0 da un error, porque no se puede dividir entre cero
             textoResultado.setText("ERROR");
         }
-       // resultado = num1/num2;
-    }// Fin división
+
+    }// Fin división*/
 
 
     public void cargarNumeros(){
